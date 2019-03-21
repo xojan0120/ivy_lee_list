61c9f93bfbde714d900a6c65e4846a9efdfbe898
xojan0120 <xojan0120@gmail.com>
2019-03-21 09:43:24 +0900

    [Update] APIのエンドポイントを環境変数で切り替えるよう変更

--- src/components/Common.js
+++ src/components/Common.js
@@ -3,8 +3,7 @@
 // ----------------------------------------------------------------------------------------
 export const cmnPreloaderSize  = 21; 
 export const cmnSeparatorId    =  1; // DB上のidと同値にする 
-export const cmnApiEndPointUri =  'http://192.168.11.7:3001/api/v1/ill';
-//export const cmnApiEndPointUri =  'http://ec2-52-68-68-224.ap-northeast-1.compute.amazonaws.com/api/v1/ill';
+export const cmnApiEndPointUri =  process.env.REACT_APP_API_ENDPOINT_URI;
 
 // ----------------------------------------------------------------------------------------
 // * Methods
